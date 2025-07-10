bae29e8e21ac216ad8cb8171342fafc381cfc5d6
joao41 <jfigueiredo095@gmail.com>
2025-07-10 15:39:25 +0100

    brucrat oko

--- Cpp05/ex00/Bureaucrat.hpp
+++ Cpp05/ex00/Bureaucrat.hpp
@@ -4,29 +4,39 @@
 #define BUREAUCRAT_HPP
 
 
+#include <cstddef>
+#include <exception>
 #include <ostream>
 #include <string>
+
 class Bureaucrat
 {
 
 	private:
-		const std::string name;
-		int rank;
-		void setgrad();
-
+		const std::string _name;
+		size_t		  _grade;
+		void setgrad(size_t grade);
 	public:
-		Bureaucrat(std::string name,int rank);
-		Bureaucrat(Bureaucrat &copy);
+		Bureaucrat(std::string name,size_t grade);
+		Bureaucrat(const Bureaucrat &copy);
 		~Bureaucrat();
 		Bureaucrat &operator=(Bureaucrat &copy);
+		std::string 	getName() const ;
+		size_t		getGrade() const ;
+		void 		increment();
+		void 		decrement();
 
-
-
-
-
+	class GradeTooHighException : public std::exception
+	{
+		public:
+			virtual	const char *what()const throw();
+	};
+	class GradeTooLowException : public std::exception
+	{
+		public:
+			virtual const char *what()const throw();
+	};
 };
 
-std::ostream &operator <<(std::ostream &o,Bureaucrat &copy);
-
-
+std::ostream &operator << (std::ostream &o,Bureaucrat *copy);
 #endif
